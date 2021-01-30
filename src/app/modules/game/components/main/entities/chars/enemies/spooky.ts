c4738d34c4a7b8afabf0812349daa8b09266db19
Adam <adam91mko@gmail.com>
2021-01-30 21:36:17 +0100

    spooky

---
+++ src/app/modules/game/components/main/entities/chars/enemies/spooky.ts
@@ -0,0 +1,7 @@
+import { Enitty } from "../../entity";
+
+export class Spooky extends Enitty {
+    constructor(scene, xPosition, yPostion, key, type){
+        super(scene, xPosition, yPostion, key, type);
+    }
+}
