44eb3631aba3e1a58979878f8bea0a2f9e8af44e
Adam <adam91mko@gmail.com>
2021-01-31 20:30:11 +0100

    inherit

--- src/app/modules/game/components/main/model/layer.model.ts
+++ src/app/modules/game/components/main/model/layer.model.ts
@@ -1,10 +1,10 @@
 export interface Layer{
-    groundLayer: Phaser.Tilemaps.TilemapLayer;
-    grassLayer: Phaser.Tilemaps.TilemapLayer;
-    treesLayer: Phaser.Tilemaps.TilemapLayer;
-    treesGrassLayer: Phaser.Tilemaps.TilemapLayer;
-    treesBackLayer: Phaser.Tilemaps.TilemapLayer;
-    woodLayer: Phaser.Tilemaps.TilemapLayer;
-    woodCollisionLayer: Phaser.Tilemaps.TilemapLayer;
-    decorativeLayer: Phaser.Tilemaps.TilemapLayer;
+    groundLayer?: Phaser.Tilemaps.TilemapLayer;
+    grassLayer?: Phaser.Tilemaps.TilemapLayer;
+    treesLayer?: Phaser.Tilemaps.TilemapLayer;
+    treesGrassLayer?: Phaser.Tilemaps.TilemapLayer;
+    treesBackLayer?: Phaser.Tilemaps.TilemapLayer;
+    woodLayer?: Phaser.Tilemaps.TilemapLayer;
+    woodCollisionLayer?: Phaser.Tilemaps.TilemapLayer;
+    decorativeLayer?: Phaser.Tilemaps.TilemapLayer;
 }
